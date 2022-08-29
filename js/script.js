870e0168e8fbd9bfb995fd26f018442eefe28f96
Oleh Zazhyrko <kosovo1989@gmail.com>
2022-08-29 22:28:36 +0200

    changed VIdeo Section

--- js/script.js
+++ js/script.js
@@ -1,36 +1,43 @@
-document.addEventListener('DOMContentLoaded', function (){
+function showVideoInfo(id) {
+	const menuSections = document.querySelectorAll(".video-menu-section");
+	const menuTabs = document.querySelectorAll(".video-menu-tab");
 
-    const nav = document.querySelector('.navbar')
-    const allNavItems = document.querySelectorAll('.nav-link')
-    const navList = document.querySelector('.navbar-collapse')
-    const navButton = document.querySelector('.navbar-toggler')
-    // const videoClick = document.getElementById('video-wrap')
+	menuSections.forEach((section) => (section.style.display = "none"));
+	menuTabs.forEach((tab) => tab.classList.remove("video-menu-tab-active"));
+	document.getElementById(id).style.display = "block";
 
-    function addShadow() {
-        if (window.scrollY >= 50) {
-            nav.classList.add('shadow-bg');
-        } else {
-            nav.classList.remove('shadow-bg');
-        }
-    }
+	const currentActiveBtn = document.querySelector(`[data-id=${id}]`);
 
-    function addShadowNavBar() {
-        nav.classList.add('shadow-bg');
-        // if (nav.classList.contains('shadow-bg')) {
-        //     nav.classList.remove('shadow-bg');
-        // } else {
-        // }
-    }
+	currentActiveBtn.classList.add("video-menu-tab-active");
+}
 
-    // function hideVideo() {
-        
-    // }
+document.addEventListener("DOMContentLoaded", function () {
+	const nav = document.querySelector(".navbar");
+	const allNavItems = document.querySelectorAll(".nav-link");
+	const navList = document.querySelector(".navbar-collapse");
+	const navButton = document.querySelector(".navbar-toggler");
+	// const videoClick = document.getElementById('video-wrap')
 
-    allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
+	function addShadow() {
+		if (window.scrollY >= 50) {
+			nav.classList.add("shadow-bg");
+		} else {
+			nav.classList.remove("shadow-bg");
+		}
+	}
 
+	function addShadowNavBar() {
+		nav.classList.add("shadow-bg");
+		// if (nav.classList.contains('shadow-bg')) {
+		//     nav.classList.remove('shadow-bg');
+		// } else {
+		// }
+	}
 
+	allNavItems.forEach((item) =>
+		item.addEventListener("click", () => navList.classList.remove("show"))
+	);
 
-
-    window.addEventListener('scroll', addShadow);
-    nav.addEventListener('click', addShadowNavBar);
-})
+	window.addEventListener("scroll", addShadow);
+	nav.addEventListener("click", addShadowNavBar);
+});
